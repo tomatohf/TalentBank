589f5da32a6bb47d64d1889d3d4d13f1526f0afa
Tomato <Tomato.HF@gmail.com>
2009-10-30 17:00:25 +0800

    none

---
+++ config/initializers/think_sphinx.rb
@@ -0,0 +1,37 @@
+require "thinking_sphinx"
+
+
+# added by Tomato
+# 
+# to make thinking sphinx recognize the parameter: charset_dictpath
+# and build its value into sphinx configure file
+# 
+# we have to use this kind of *hack* way
+# since *charset_dictpath* is not a sphinx parameter, but a Coreseek's
+# for Chinese word segmentation
+
+module Riddle
+  class Configuration
+    class Section
+      
+      alias original_settings_body settings_body
+      
+      private
+      
+      def settings_body
+        charset_dictpath = if ThinkingSphinx::Configuration.environment == "production"
+          "/home/tomato/websites/app/CareerCommunity/shared/sphinx/dict"
+        else
+          "/Users/tomato/Dev/git/projects/CareerCommunity/db/sphinx/dict"
+        end
+        
+        if self.class.name == "Riddle::Configuration::Index"
+          original_settings_body << "  charset_dictpath = #{charset_dictpath}"
+        else
+          original_settings_body
+        end
+      end
+      
+    end
+  end
+end
