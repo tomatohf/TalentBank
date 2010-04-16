bf12c51b32141ea0d56d14ce7e3bcbfaac3d41e4
Tomato <Tomato.HF@gmail.com>
2010-04-16 11:05:38 +0800

    none

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -13,11 +13,12 @@ class Skill < HashModel::Simple
     [
       {
         :id => 100100,
-        :name => "CPA证书",
+        :name => "大学英语四级(CET4)",
         :value_type => "level",
         :data => [
           {:value => 0, :label => ""},
-          {:value => 10, :label => "通过"}
+          {:value => 10, :label => "通过"},
+          {:value => 20, :label => "优秀"}
         ]
       },
       
@@ -36,26 +37,139 @@ class Skill < HashModel::Simple
       
       {
         :id => 100300,
-        :name => "上海计算机等级考试",
+        :name => "专业英语四级(TEM4)",
         :value_type => "level",
         :data => [
           {:value => 0, :label => ""},
-          {:value => 10, :label => "1 级"},
-          {:value => 20, :label => "2 级"},
-          {:value => 30, :label => "3 级"}
+          {:value => 10, :label => "通过"},
+          {:value => 20, :label => "优秀"}
         ]
       },
       
       
       {
         :id => 100400,
-        :name => "乔布堂销售技能培训",
+        :name => "专业英语八级(TEM8)",
         :value_type => "level",
         :data => [
           {:value => 0, :label => ""},
-          {:value => 10, :label => "毕业"}
+          {:value => 10, :label => "通过"},
+          {:value => 20, :label => "优秀"}
         ]
-      }
+      },
+      
+      
+      {
+        :id => 100500,
+        :name => "全国计算机等级证书",
+        :value_type => "level",
+        :data => [
+          {:value => 0, :label => ""},
+          {:value => 10, :label => "一级"},
+          {:value => 20, :label => "二级"},
+          {:value => 30, :label => "三级"},
+          {:value => 40, :label => "四级"}
+        ]
+      },
+      
+      
+      {
+        :id => 100600,
+        :name => "计算机技术与软件专业技术资格",
+        :value_type => "level",
+        :data => [
+          {:value => 0, :label => ""},
+          {:value => 10, :label => "初级"},
+          {:value => 20, :label => "中级"},
+          {:value => 30, :label => "高级"}
+        ]
+      },
+      
+      
+      {
+        :id => 100700,
+        :name => "日本语能力测试(JLPT)",
+        :value_type => "level",
+        :data => [
+          {:value => 0, :label => ""},
+          {:value => 10, :label => "四级"},
+          {:value => 20, :label => "三级"},
+          {:value => 30, :label => "二级"},
+          {:value => 40, :label => "一级"}
+        ]
+      },
+      
+      
+      {
+        :id => 100800,
+        :name => "证券从业资格",
+        :value_type => "level",
+        :data => [
+          {:value => 0, :label => ""},
+          {:value => 10, :label => "通过"},
+          {:value => 20, :label => "一级"},
+          {:value => 30, :label => "二级"}
+        ]
+      },
+      
+      
+      {
+        :id => 100900,
+        :name => "注册国际投资分析师(CIIA)",
+        :value_type => "level",
+        :data => [
+          {:value => 0, :label => ""},
+          {:value => 10, :label => "通过"}
+        ]
+      },
+      
+      
+      {
+        :id => 101000,
+        :name => "特许金融分析师(CFA)",
+        :value_type => "level",
+        :data => [
+          {:value => 0, :label => ""},
+          {:value => 10, :label => "初级"},
+          {:value => 20, :label => "中级"},
+          {:value => 30, :label => "高级"}
+        ]
+      },
+      
+      
+      {
+        :id => 101100,
+        :name => "报关员资格证书",
+        :value_type => "level",
+        :data => [
+          {:value => 0, :label => ""},
+          {:value => 10, :label => "通过"}
+        ]
+      },
+      
+      
+      {
+        :id => 101200,
+        :name => "注册会计师(CPA)",
+        :value_type => "level",
+        :data => [
+          {:value => 0, :label => ""},
+          {:value => 10, :label => "通过"}
+        ]
+      },
+      
+      
+      {
+        :id => 101300,
+        :name => "物流师职业资格认证",
+        :value_type => "level",
+        :data => [
+          {:value => 0, :label => ""},
+          {:value => 10, :label => "助理物流师"},
+          {:value => 20, :label => "物流师"},
+          {:value => 30, :label => "高级物流师"}
+        ]
+      },
     ]
   end
   
