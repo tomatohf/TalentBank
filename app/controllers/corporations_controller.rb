e7c6807e5a3b92f0f29be206bffdd966bed0626b
Tomato <Tomato.HF@gmail.com>
2010-09-16 16:20:33 +0800

    extract corporation profile filling code to helper method

--- app/controllers/corporations_controller.rb
+++ app/controllers/corporations_controller.rb
@@ -67,30 +67,7 @@ class CorporationsController < ApplicationController
   def update_profile
     @profile = CorporationProfile.get_record(@corporation.id)
     
-    @profile.email = params[:email] && params[:email].strip
-    @profile.phone = params[:phone] && params[:phone].strip
-    @profile.contact = params[:contact] && params[:contact].strip
-    @profile.contact_gender = case params[:contact_gender]
-      when "true"
-        true
-      when "false"
-        false
-      else
-        nil
-    end
-    @profile.contact_title = params[:contact_title] && params[:contact_title].strip
-    
-    @profile.address = params[:address] && params[:address].strip
-    @profile.zip = params[:zip] && params[:zip].strip
-    @profile.website = params[:website] && params[:website].strip
-    
-    @profile.nature_id = params[:nature]
-    @profile.size_id = params[:size]
-    @profile.industry_id = params[:industry]
-    @profile.province_id = params[:province]
-    @profile.city_id = params[:city]
-    
-    @profile.desc = params[:desc] && params[:desc].strip
+    self.class.helpers.fill_corporation_profile(@profile, params)
     
     if @profile.save
       @corporation.renew_updated_at(@profile.updated_at)
---
+++ app/helpers/corporations_helper.rb
@@ -0,0 +1,30 @@
+module CorporationsHelper
+
+  def fill_corporation_profile(profile, params)
+    profile.email = params[:email] && params[:email].strip
+    profile.phone = params[:phone] && params[:phone].strip
+    profile.contact = params[:contact] && params[:contact].strip
+    profile.contact_gender = case params[:contact_gender]
+      when "true"
+        true
+      when "false"
+        false
+      else
+        nil
+    end
+    profile.contact_title = params[:contact_title] && params[:contact_title].strip
+    
+    profile.address = params[:address] && params[:address].strip
+    profile.zip = params[:zip] && params[:zip].strip
+    profile.website = params[:website] && params[:website].strip
+    
+    profile.nature_id = params[:nature]
+    profile.size_id = params[:size]
+    profile.industry_id = params[:industry]
+    profile.province_id = params[:province]
+    profile.city_id = params[:city]
+    
+    profile.desc = params[:desc] && params[:desc].strip
+  end
+  
+end
