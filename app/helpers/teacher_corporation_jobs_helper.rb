5e73999dbe44a6c0211fa76899bb44442319fa1d
Tomato <Tomato.HF@gmail.com>
2011-03-25 18:08:01 +0800

    none

--- app/helpers/teacher_corporation_jobs_helper.rb
+++ app/helpers/teacher_corporation_jobs_helper.rb
@@ -5,13 +5,26 @@ module TeacherCorporationJobsHelper
   end
   
   
-  def sms(corporation, job)
+  def sms_common(corporation, job)
     district = JobDistrict.find(job.district_id)
     result = JobResult.find(job.result_id)
     
     %Q!#{corporation.get_name}›#{job.get_name}:#{remove_newline(job.requirement)}#{result && result[:name]}。! +
-    %Q!工作地址:#{district && "#{district[:name]}›"}#{job.place}。! +
-    "有意申请该岗位者，请于2小时内致电60517703或60516910".gsub(/\s/, "")
+    %Q!工作地址:#{district && "#{district[:name]}›"}#{job.place}。!
+  end
+  
+  
+  def sms(corporation, job)
+    sms_common(corporation, job) +
+    "有意申请该岗位者，请于2小时内致电60517703或60516910"
+  end
+  
+  
+  def sms_2(corporation, job, teacher)
+    email = teacher.email.blank? ? "student@qiaobutang.com" : teacher.email
+    
+    sms_common(corporation, job) +
+    %Q!有意申请该岗位者，请在#{format_date(job.recruit_end_at)}之前，将您的简历发送到#{email}!
   end
   
 end
